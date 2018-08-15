1e704a5af8aacdcf552941f42aad9a371e56a71b
Deepbooty <511108087@qq.com>
2018-08-15 10:33:33 +0800

    命名修改

---
+++ App/src/components/headerBar/index.vue
@@ -0,0 +1,93 @@
+<template>
+	<div class="header">
+		<div class="headerBox">
+			<div class="logo">
+				<img src="@/assets/images/logo.png" alt="">
+			</div>
+			<div class="search">
+				<i class="iconfont icon-search"></i>
+				<input type="text" placeholder="请输入搜索关键词~">
+			</div>
+			<div class="issue">
+				<i class="iconfont icon-dfabu"></i>
+				<span class="fabu">	发布</span>
+			</div> 
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+
+<style scoped>
+	.header {
+		width: 100%;
+		height: 0.44rem;
+		position: fixed;
+		top: 0;
+		z-index: 99;
+	}
+	.headerBox {
+		position: relative;
+		height: 0.44rem;
+		background-color: #ec414d;
+	}
+	.logo {
+		position: absolute;
+		left: 0.1rem;
+		width: 1.0rem;
+		height: 100%;
+	}
+	.logo img {
+		margin-top: 0.1rem
+		;
+	}
+	
+	.issue {
+		width: .5rem;
+		height: 100%;
+		position: absolute;
+		right: 0.1rem;
+		top: 0;
+		z-index: 999;
+		text-align: center;
+		font-size: 0.14rem;
+		color: #fff;
+	}
+	.issue .icon-dfabu {
+		display: block;
+		font-size: 0.16rem;
+		padding-top: 0.05rem;
+	}
+	.search {
+		width: 100%;
+		height: 0.3rem;
+		line-height: .3rem;
+		position: relative;
+		top: 0.06rem;
+		padding-left: 1.2rem;
+		padding-right: .6rem;
+	}
+	.search i {
+		width: .3rem;
+		height: .3rem;
+		/*background-color: #ff0;*/
+		position: absolute;
+		top: 0.01rem;
+		padding-left: 0.1rem;
+		font-size: .16rem;
+		color: #999;
+	}
+	.search input {
+		width: 100%;
+		height: 100%;
+		padding-left: 0.3rem;
+		padding-right: 0.1rem;
+		background-color: #fff;
+		border-radius: 0.14rem;
+		font-size: 0.14rem;
+	}
+
+</style>
---
+++ App/src/components/navBar/index.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="footer-nav">
+		<div class="footer-menu">
+			<div class="footer-item">
+				<i class="iconfont icon-index footer-icon"></i>
+				<span class="footer-text">首页</span>
+			</div>
+			<div class="footer-item">
+				<i class="iconfont icon-xiaoxi footer-icon"></i>
+				<span class="footer-text">消息</span>
+			</div>
+			<div class="footer-item">
+				<i class="iconfont icon-video footer-icon"></i>
+				<span class="footer-text">视频</span>
+			</div>
+			<div class="footer-item">
+				<i class="iconfont icon-wode footer-icon"></i>
+				<span class="footer-text">我的</span>
+			</div>
+		</div>
+	</div>
+</template>
+<style lang="css" scoped>
+	.footer-nav {
+		width: 100%;
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		text-align: center;
+		font-size: 0.12rem;
+		overflow: hidden;
+		background-color: #fff;
+		box-shadow: 0 0 5px #ccc;
+		height: 0.5rem;
+	}
+	.footer-item {
+		display: block;
+		float: left;
+		width: 25%;
+		height: 100%;
+		color: #999;
+	}
+	.footer-icon {
+		display: block;
+		font-size: 0.22rem;
+		padding-top: 0.04rem;
+		padding-bottom: .02rem;
+	}
+</style>
