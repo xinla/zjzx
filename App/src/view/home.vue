fe7069e7722cbde42b5134db85e0108c35107259
Deepbooty <511108087@qq.com>
2018-08-15 10:32:58 +0800

    删除 添加

---
+++ App/src/view/home.vue
@@ -0,0 +1,24 @@
+<!-- 首页新闻页面 -->
+<template>
+	<div>
+		<z-header></z-header>
+		<z-tab></z-tab>
+		<z-nav></z-nav>
+		<!-- <z-swiper></z-swiper> -->
+	</div>
+</template>
+ 
+<script>
+	import zHeader from '@/components/headerBar/index'
+	import zTab from '@/components/tabBar/index'
+	// import zSwiper from '@/components/Swiper/index'
+	import zNav from '@/components/navBar/index'
+	export default {
+		components:{
+			zHeader,
+			zTab,
+			// zSwiper
+			zNav
+		}
+	}
+</script>
