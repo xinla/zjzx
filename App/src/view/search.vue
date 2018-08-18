5d4aa63ed3850757b16315c977f7071b45a35389
Deepbooty <511108087@qq.com>
2018-08-18 09:57:32 +0800

    search 页面

--- App/src/view/search.vue
+++ App/src/view/search.vue
@@ -8,33 +8,56 @@
 			<span class="cancle">取消</span>
 		</div>
 		<div class="search-key">
-			<a href="javascript:void(0)">池州新闻</a>
-			<a href="javascript:void(0)">池州房价</a>
-			<a href="javascript:void(0)">皖南古村</a>
+			<a href="javascript:void(0)" v-for="(item,index) in keyList ">{{item}}</a>
 		</div>
 		<div class="search-history">
-			<div class="search-history-head">
-				<span>历史记录</span>
-				<i class="iconfont icon-like"></i>
+			<div class="search-head clearfix">
+				<span class="fl">历史记录</span>
+				<i class="iconfont fr">&#xe646;</i>
 			</div>
-			<div class="search-history-body">
-				<a href="javascript:void(0)">池州新闻</a>
-				<a href="javascript:void(0)">池州新闻</a>
-				<a href="javascript:void(0)">池州新闻</a>
+			<div class="search-body">
+				<ul class="search-list clearfix">
+					<li v-for="item in historyList"><a href="javascript:void(0)" >{{item.historytext}}</a></li>
+				</ul>
+			
 			</div>
 		</div>
-		<!-- <div class="search-history">
-			<span>搜索历史</span>
-			<span>清空</span>
+
+		<div class="search-history">
+			<div class="search-head clearfix">
+				<span class="fl">猜你想搜的</span>
+				<i class="iconfont fr">&#xea2a;</i>
+			</div>
+			<div class="search-body">
+				<ul class="search-list clearfix">
+					<li v-for="item in guessList"><a href="javascript:void(0)">{{item.guesstext}}</a></li>
+				</ul>
+			</div>
 		</div>
-		<span class="search-span">内容内容内容</span>
-		<div class="hot-search">
-			<span>热门搜索</span>
-		</div>
-		<span class="search-span">内容内容内容</span> -->
 	</div>
 </template>
 
+<script>
+	export default {
+		data() {
+			return {
+				keyList: ['关键词1','关键词2','关键词3'],
+				historyList:[
+					{historytext:'历史记录1'},
+					{historytext:'历史记录2'},
+					{historytext:'历史记录3'},
+					{historytext:'历史记录4'},
+				],
+				guessList: [
+					{guesstext: '搜你想搜的1'},
+					{guesstext: '搜你想搜的2'},
+					{guesstext: '搜你想搜的3'},
+					{guesstext: '搜你想搜的4'},
+				]
+			}
+		}
+	}
+</script>
 <style scoped>
 	.search-container {
 		height: 3.5em;
@@ -58,13 +81,26 @@
 		z-index: 999;
 		height: 100%;
 	}
+	.search-wrap .iconfont {
+		color: #999;
+	}
 	[data-dpr="1"] .iconfont {
-		font-size: 24px;
+		font-size: 14px;
 	}
 	[data-dpr="2"] .iconfont {
-		font-size: 48px;
+		font-size: 28px;
 	}
 	[data-dpr="3"] .iconfont {
+		font-size: 42px;
+	}
+
+	[data-dpr="1"] .search-icon {
+		font-size: 24px;
+	}
+	[data-dpr="2"] .search-icon {
+		font-size: 48px;
+	}
+	[data-dpr="3"] .search-icon {
 		font-size: 72px;
 	}
 	
@@ -83,13 +119,13 @@
 		vertical-align: middle;
 	}
 	[data-dpr="1"] .cancle {
-		font-size: 16px;
+		font-size: 18px;
 	}
 	[data-dpr="2"] .cancle {
-		font-size: 32px;
+		font-size: 36px;
 	}
 	[data-dpr="3"] .cancle {
-		font-size: 48px;
+		font-size: 54px;
 	}
 
 
@@ -109,6 +145,15 @@
 	.search-key a:last-child{
 		border: none;
 	}
+	[data-dpr="1"] .search-key a {
+		font-size: 16px;
+	}
+	[data-dpr="2"] .search-key a {
+		font-size: 32px;
+	}
+	[data-dpr="3"] .search-key a {
+		font-size: 48px;
+	}
 
 
 	.search-history {
@@ -117,10 +162,45 @@
 		border-top: .04em solid #e8e8e8;
 		border-bottom: .04em solid #e8e8e8; 
 	}
-	.search-history-head {
+	.search-head,
+	.search-body {
 		padding: 0 1.25em;
 		height: 3.3em;
 		line-height: 3.3em;
 		border-bottom: .04em solid #e8e8e8; 
 	}
+	.search-head span{
+		color: #999;
+	}
+	[data-dpr="1"] .search-head span{
+		font-size: 14px;
+	}
+	[data-dpr="2"] .search-head span {
+		font-size: 28px;
+	}
+	[data-dpr="3"] .search-head span {
+		font-size: 42px;
+	}
+	.search-body {
+		padding: 0;
+		height: auto;
+		border: 0;
+	}
+	.search-list li {
+		width: 50%;
+		min-width: 45%;
+		float: left;
+		padding-left: 1.25em;
+		border-right: .04em solid #e8e8e8; 
+		border-bottom: .04em solid #e8e8e8; 
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+	}
+	.search-list li:last-child{
+		border-right: none;
+	}
+
+	
+
 </style>
