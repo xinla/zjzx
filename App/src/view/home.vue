bbeb2fc731b07bda52516e5ffbb6a1572e6b8b8d
xinla <1570728529@qq.com>
2018-09-25 21:48:37 +0800

    首页构思调整

--- App/src/view/home.vue
+++ App/src/view/home.vue
@@ -3,13 +3,44 @@
 	<div>
 		<div class="index" v-show="1 == showIndex">
 			<home-header></home-header>
-			<home-nav></home-nav>
+			<div class="main-wrap">
+				<div class="bfc-o">
+					<tab :line-width=2 active-color='#fc378c' v-model="classifyIndex">
+						<tab-item :selected="currentClassiftyName == item.classifyname" v-for="(item,index) in classifyList" @click="currentClassiftyName = item.classifyname" :key="index">{{item.classifyname}}
+						</tab-item>
+					</tab>
+					<router-link to="/more">
+						<div class="nav-add">
+							<i class="iconfont">&#xe610;</i>
+						</div>
+					</router-link>					
+				</div>
 			<div class="main">
 				<loading-main v-show="ifLoad"></loading-main>
-				<keep-alive>
-					<router-view></router-view>		
-					
-				</keep-alive>
+				<swiper v-model="classifyIndex" height="500px" :show-dots="false">
+			        <swiper-item>
+			          	<div class="main-content" @scroll="loadMore">
+			          		<multIT v-for="(item,index) in (0 == classifyIndex?arcListA:[])" :article="item" :ifPublisher="true" :key="index"></multIT>
+			          	</div>
+			        </swiper-item>
+			        <swiper-item>
+			          	<div class="main-content" @scroll="loadMore">
+			          		<multIT v-for="(item,index) in (1 == classifyIndex?arcListB:[])" :article="item" :ifPublisher="true" :key="index"></multIT>
+			          	</div>
+			        </swiper-item>
+			        <swiper-item>
+			          	<div class="main-content" @scroll="loadMore">
+			          		<multIT v-for="(item,index) in (2 == classifyIndex?arcListA:[])" :article="item" :ifPublisher="true" :key="index"></multIT>
+			          	</div>
+			        </swiper-item>
+			        <swiper-item>
+			          	<div class="main-content" @scroll="loadMore">
+			          		<multIT v-for="(item,index) in (3 == classifyIndex?arcListB:[])" :article="item" :ifPublisher="true" :key="index"></multIT>
+			          	</div>
+			        </swiper-item>
+		      </swiper>
+			</div>
+	
 			</div>
 		</div>
 		<member v-show="4 == showIndex"></member>
@@ -37,22 +68,35 @@
 </template>
 
 <script>	
-	// import config from '@/lib/config/config'
-	import homeHeader from '@/components/headerBar'
-	import homeMore from'@/components/more'
-	import homeNav from '@/components/swiperNav'
-	import bottomNav from '@/components/bottomNav'
-	import member from '@/view/member/index'
+// import config from '@/lib/config/config'
+import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem } from 'vux'
+import homeHeader from '@/components/headerBar'
+import homeMore from'@/components/more'
+import homeNav from '@/components/swiperNav'
+import bottomNav from '@/components/bottomNav'
+import multIT from '@/components/news/multIT'
 
-	import homeService from '@/service/homeService'
-	import articleService from '@/service/articleService'
+import member from '@/view/member/index'
+
+import recommend from '@/view/part/recommend'
+// import fangPian from '@/view/part/fangPian'
+
+
+import homeService from '@/service/homeService'
+import articleService from '@/service/articleService'
+import articleClassifyService from '@/service/article_classifyService'
 
 	export default {
 		components:{
 			homeHeader,
 			homeNav,
 			member,
-			homeMore
+			homeMore,
+			Tab,
+			TabItem,
+			Swiper, 
+			SwiperItem,
+			multIT,
 		},
 		created () {
 			// debugger;
@@ -65,6 +109,13 @@
 			return {
 				ifLoad:false,
 				showIndex:1,
+				classifyList:[],
+				arcListA:[],
+				arcListB:[],
+				classifyIndex:0,
+				currentClassiftyName:"推荐",
+				pageSize:1,
+				lock:false,
 			}
 		},
 		mounted () {
@@ -73,6 +124,16 @@
 	    	// window.addEventListener(resizeEvt, this.recalc, false);
 	    	// document.addEventListener('DOMContentLoaded', this.recalc, false);
 	    	// this.recalc();
+	    	// 
+	    	articleClassifyService.getArticleClassifyList(data=>{
+				if (data && data.status == "success") {
+					this.classifyList = data.result.classfyList;					
+					// console.log(this.classifyList)
+				}
+			});
+
+			this.$options.methods.getArtList.call(this);
+			this.ifLoad = false;
 	    },
 		methods:{
 			// recalc(){
@@ -138,7 +199,34 @@
 	  //       },
 	        show(whi){
 	        	this.showIndex = whi;
-	        }
+	        },
+	        getArtList(){
+				this.lock = true;
+				let resArticlePage = articleService.articlePage(this.pageSize,15);
+				if (resArticlePage && resArticlePage.status == "success") {
+					this.arcListA = this.arcListA.concat(resArticlePage.recordPage.list);
+					// this.arcListB = this.arcListB.concat(resArticlePage.recordPage.list);	
+					this.pageSize++;
+					this.lock = false;
+					// console.log(this.arcList);articlePage
+				}
+			},
+			loadMore(e){
+				if (!this.lock && ($(e.target).scrollTop() + $(e.target).height()) > e.target.scrollHeight-350) {
+					this.$options.methods.getArtList.call(this);
+					// console.log(1)
+				}
+				// let that = this;
+				// $(".main-content").scroll(function(){
+				// 	if ( ($(this).scrollTop() + $(this).height()) > $(this)[0].scrollHeight-350) {
+				// 	that.$options.methods.getArtList.call(that);
+				// // 	console.log(that)
+				// // console.log(this)
+				// }
+				// })
+				// console.log(e.target)
+			}
+
 	    },
 	    
 	    destroyed(){
@@ -170,12 +258,18 @@
 	    // }
 	}
 </script>
-<style scoped>
-	.main{
-		padding:6em 0.5em 3.5em;
+<style lang="less" scoped>
+	.main-wrap{
+		padding:3em 0px 0;
 		height: 100vh;
+		width: 100%;
 		background: #fff;
-		overflow-x: hidden;
+		overflow: hidden;
+	}
+	.main{
+		height: calc(100% - 90px);
+		padding:0 0.5em;
+		overflow-y:auto;
 	}
 	.footer-nav {
 		width: 100%;
@@ -201,4 +295,41 @@
 	.footer-item a.router-link-active,.current{
 		color: #f40;
 	}
+	.nav-add {
+		position: absolute;
+		right: 0;
+		top: 0;
+		width: .72rem;
+		height: @navHeight;
+		line-height: @navHeight;
+		text-align: center;
+		background-color: rgba(255,255,255,.8);
+		box-shadow:-.1rem 0px .4rem #cccccc;
+		color:#000;
+		i {
+			font-size: .40rem;
+		}
+	}
+	.main-content{
+		height: 100%;
+		overflow-y: auto;
+	}
+	.router-link-active{
+		color:@currentColor;
+	}
+	/*vux 样式修改*/
+	.vux-tab-wrap{
+      	padding-top: 38px;
+	}
+	.vux-tab .vux-tab-item{
+		line-height: 38px;
+	}
+	.vux-tab-container{
+		height: 38px;
+	}
+</style>
+<style>
+	.vux-tab{
+		height: 38px !important;
+	}	
 </style>
